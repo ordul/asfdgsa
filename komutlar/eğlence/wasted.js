64a4e0ef30c75a43a37f174d2f4e916c9cc64a05
ordul <74411150+ordul@users.noreply.github.com>
2021-05-03 21:40:24 +0300

    dasd

--- komutlar/eğlence/wasted.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name:"wasted",
-    code:`
-    $replaceText[https://some-random-api.ml/canvas/wasted?avatar=$authorAvatar;.webp;.png;-1]
-    
-    `
-
-}
--- komutlar/kayıt/üye-kayıt-sıfırla.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name:"üye-kayıt-sıfırla",
-    code:`
-    Kayıt Sistemi Resetlenmiştir !
-    $resetServerVar[üyerol]
-    $resetServerVar[kkanal]
-    $onlyIf[$hasPerms[$authorID;admin]!=false;Üzgünüm Bunu Sadece \`\`Yönetici\`\` Kişileri Ayarlayabilir !]
-    `
-  }
